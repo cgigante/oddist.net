4df0ca8795fe2c3f00ca69e50ec30b1e9cbeb90c
Charlie Gigante <charlie.gigante@gmail.com>
2016-02-22 11:45:16 -0500

    assorted minor updates

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>ODDIST</title>
+    <title>ODDIST | Electronic Music Producer</title>
     <link rel="shortcut icon" href="images/icon.png">
     <meta name="description" content="ODDIST is an electronic music producer from New York City." />
     <meta name="keywords" content="ODDIST, musician, music, band, artist, dj, dub, bass music, footwork, house, dubstep, new york city, producer, music, brooklyn, nyc">
@@ -62,31 +62,43 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a id="oddist-text-logo" class="navbar-brand active" href="index.html">ODDIST</a>
+              <a class="navbar-brand active" href="index.php"><img id="nav-bar-logo" class="img-logo" src="images/oddist-signature_white.png" alt="ODDIST logo" title="ODDIST logo" /></a>
             </div>
 
             <div class="collapse navbar-collapse">
               <ul class="nav navbar-nav">
-                <li id="music-icon-id"><a href="#listen" class="navCustom">LISTEN</a></li>
-                <!-- <li><a href="#philosophy" class="navCustom">PHILOSOPHY</a></li>
-                <li><a href="#support" class="navCustom">SUPPORT</a></li> -->
+                <!-- <li id="music-icon-id"><a href="#listen" class="navCustom">LISTEN</a></li> -->
                 <li><a href="#friends" class="navCustom">FRIENDS</a></li>
+                <li><a href="#thank-you" class="navCustom">CONTACT</a></li>
                 <!-- BEGIN DROPDOWN -->
                 <li class="dropdown"> 
-                  <a href="#" class="dropdown-toggle navCustom" data-toggle="dropdown">LINKS<b class="caret"></b></a>
+                  <a href="#" class="dropdown-toggle navCustom" data-toggle="dropdown">MUSIC<b class="caret"></b></a>
                   <ul class="dropdown-menu">
-                  	<li><a href="http://oddist.bandcamp.com/" target="_blank">MY MUSIC ON BANDCAMP</a></li>
+                  	<li><a href="http://oddist.bandcamp.com/" target="_blank">BANDCAMP</a></li>
                     <li><a href="https://soundcloud.com/oddist" target="_blank">SOUNDCLOUD</a></li>
-                    <li><a href="https://www.facebook.com/ODDISTMUSIC" target="_blank">FACEBOOK</a></li>
                     <li><a href="http://8tracks.com/the-oddfather" target="_blank">8TRACKS</a></li>
-                    <li><a href="http://www.last.fm/user/oddist" target="_blank">LAST.FM</a></li>
-										<li><a href="http://bandcamp.com/oddist#wishlist" target="_blank">WISHLIST</a></li>
+
+					<!-- <li><a href="http://bandcamp.com/oddist#wishlist" target="_blank">WISHLIST</a></li> -->
                   </ul>
                 </li>
                 <!-- END DROPDOWN -->
-                <!-- <li><a href="mailto:oddistmusic@gmail.com" class="navCustom">CONTACT</a></li> -->
-                <li><a href="#thank-you" class="navCustom">CONTACT</a></li>
+                
               </ul>
+
+              <ul class="nav navbar-nav navbar-right">
+                <!-- <li><a href="http://oddist.bandcamp.com/" target="_blank">BANDCAMP</a></li> -->
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SOCIAL<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="https://www.facebook.com/ODDISTMUSIC" target="_blank">FACEBOOK</a></li>
+                    <li><a href="http://www.last.fm/user/oddist" target="_blank">LAST.FM</a></li>
+                    <!-- <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li> -->
+                  </ul>
+                </li>
+              </ul>
+
+
             </div><!--/.nav-collapse -->
           </div> <!-- End container -->
         </div>  <!-- End Nav Bar -->
@@ -106,29 +118,53 @@
             <div class="break1"></br></div>
             <div class="col-md-12">
               <div class="home-div animated fadeIn">
-                <div class="col-md-4"></div>
-                <div class="col-md-4">
+                <div class="col-md-1"></div>
+                <div class="col-md-10">
                   <center>
-                    <h3 class="bio"><img src="images/oddist-signature_black.png" alt="ODDIST signature" title="ODDIST signature" style="width:70px"> is an electronic music producer from </br>New York City</h3>
+                    <h3 class="bio">
+                    <!-- <img src="images/oddist-signature_black.png" alt="ODDIST signature" title="ODDIST signature" style="width:70px"> -->
+                      <!-- <span id="textLogo">ODDIST is an electronic music producer</span> -->
+                    </h3>
                   <center>
                 </div>
-                <div class="col-md-4"></div>
+                <div class="col-md-1"></div>
               </div>
             </div> 
           </div> 
 
           <div class="row">
-            <!-- <div class="col-md-2"></div> -->
-            <div class="col-md-12">
-              <img class="img-responsive" src="images/gif/flowers.gif" alt="flowers animation">        
+
+            <div class="col-md-6">
+              <br>
+              
+              <center>
+                <p>New album, <a href="https://oddist.bandcamp.com/album/cant-fail-me-now" target="_blank">CAN'T FAIL ME NOW</a>, available now:</p>
+                <iframe style="border: 0; width: 350px; height: 350px;" src="https://bandcamp.com/EmbeddedPlayer/album=2325122358/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/transparent=true/" seamless><a href="http://oddist.bandcamp.com/album/cant-fail-me-now">CAN&#39;T FAIL ME NOW by ODDIST</a></iframe>
+              <center>   
+
             </div>
+
+            <div class="col-md-6">
+              <br>
+              
+              <center>
+                <p>New EP, <a href="https://oddist.bandcamp.com/album/not-every-door-is-yours-ep" target="_blank">NOT EVERY DOOR IS YOURS</a>, available now:</p>
+                <iframe style="border: 0; width: 350px; height: 350px;" src="https://bandcamp.com/EmbeddedPlayer/album=1169613114/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/transparent=true/" seamless><a href="http://oddist.bandcamp.com/album/not-every-door-is-yours-ep">NOT EVERY DOOR IS YOURS EP by ODDIST</a></iframe>
+              <center>
+
+            </div>
+
+
+
+
+
             <!-- <div class="col-md-2"></div> -->
           </div>
 
           <div class="row">            
             <div class="col-md-12">
               <center>
-                <p class="description" id="scrolldowntolisten"><a href="#listen"><span class="glyphicon glyphicon-chevron-down"></span></a></p>
+                <p class="description" id="scrolldowntolisten"><a href="#friends"><span class="glyphicon glyphicon-chevron-down"></span></a></p>
               </center>
             </div>
           </div>
@@ -142,97 +178,9 @@
 
 
 
-<!-- ############# BEGIN LISTEN ################# -->
-    
-    <div class="break" id="listen"></div>
-
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="home-div animated fadeIn">
-                <div class="col-md-12">
-                  <center><p class="description">Listen.</p></center>
-                  <iframe id="soundcloud-iframe" width="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/2415232&amp;color=5d478b&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
-                </div>
-
-                <div class="col-md-12">
-                  <center>
-                    <p class="description soundcloud-p" id="scrolluptoindex"><a href="#index"><span class="glyphicon glyphicon-chevron-up"></span></a></p>
-                    <p class="description soundcloud-p">&nbsp;&nbsp;</p>
-                    <p class="description soundcloud-p" id="scrolldowntophilosophy"><a href="#friends"><span class="glyphicon glyphicon-chevron-down"></span></a></p>
-                  </center>
-                </div>
-              </div>
-            </div>
-            <div class="clear"></div>
-          </div>
-        </div>
-
-<!-- ############# END LISTEN ################# -->
-
-
-<!-- begin image divider  -->
-        <!-- <div class="container">
-          <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-8 filler-div">
-              <img src="images/slide1.jpg" class="img-responsive" alt="">             
-            </div>
-            <div class="col-md-2"></div>
-          </div> 
-        </div>  -->
-<!-- End image divider  -->
 
 
 
-<!-- begin image divider  -->
-        <!-- <div class="container">
-          <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-8 filler-div">
-              <img src="images/slide6.jpg" class="img-responsive" alt="">             
-            </div>
-            <div class="col-md-2"></div>
-          </div>
-        </div> -->
-<!-- End image divider  -->
-
-
-<!-- begin image divider  -->
-        <!-- <div class="container">
-          <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-8 filler-div">             
-              <img src="images/slide7.jpg" class="img-responsive" alt="">             
-            </div>
-            <div class="col-md-2"></div>
-          </div>
-        </div>  -->
-<!-- End image divider  -->
-
-<!-- begin image divider  -->
-        <!-- <div class="container">
-          <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-8 filler-div">             
-              <img src="images/slide4.jpg" class="img-responsive" alt="">             
-            </div>
-            <div class="col-md-2"></div>
-          </div>
-        </div>  -->
-<!-- End image divider  -->
-
-<!-- begin image divider  -->
-        <!-- <div class="container">
-          <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-8 filler-div">             
-              <img src="images/slide10.jpg" class="img-responsive" alt="">             
-            </div>
-            <div class="col-md-2"></div>
-          </div> 
-        </div> -->
-<!-- End image divider  -->
 
 <!-- ############# BEGIN FRIENDS ################# -->
         
@@ -244,21 +192,22 @@
 
               <div class="home-div animated fadeIn">
                 <center>
-                    <p class="description">Friends &amp; Inspiration:</p>
-                    <p class="p-liststyle"><a target="_blank" href="http://www.dublab.com">dublab</a></p>
-                    <p class="p-liststyle"><a target="_blank" href="http://www.paxicorecords.com">Paxico Records</a></p>
-                    <p class="p-liststyle"><a target="_blank" href="http://rudypalos.bandcamp.com">Rudy Palos</a></p>
-                    <p class="p-liststyle"><a target="_blank" href="http://www.hyperdub.net">Hyperdub</a></p>
-                    <p class="p-liststyle"><a target="_blank" href="http://kindercorerecords.blogspot.com">Kindercore Records</a></p>
-                    <p class="p-liststyle"><a target="_blank" href="http://www.desirerecords.com">Desire Records</a></p>
-                    <p class="p-liststyle"><a target="_blank" href="https://www.dustygroove.com">Dusty Groove Records</a></p>
-                    <p class="p-liststyle"><a target="_blank" href="http://minimalwave.com">Minimal Wave</a></p>
-                    <p class="p-liststyle"><a target="_blank" href="http://www.dischord.com">Dischord Records</a></p>
-                    <p class="p-liststyle"><a target="_blank" href="http://www.souljazzrecords.co.uk/">Soul Jazz Records</a></p>
+                  <p class="friendsDescription">Friends &amp; Inspiration:</p>
+                  <p class="p-liststyle"><a target="_blank" href="http://www.dublab.com">dublab</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="http://www.paxicorecords.com">Paxico Records</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="http://rudypalos.bandcamp.com">Rudy Palos</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="http://niljoy.com">Niljoy</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="http://www.hyperdub.net">Hyperdub</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="http://kindercorerecords.blogspot.com">Kindercore Records</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="http://www.desirerecords.com">Desire Records</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="https://www.dustygroove.com">Dusty Groove Records</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="http://minimalwave.com">Minimal Wave</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="http://www.dischord.com">Dischord Records</a></p>
+                  <p class="p-liststyle"><a target="_blank" href="http://www.souljazzrecords.co.uk/">Soul Jazz Records</a></p>
 
-                    <p class="description soundcloud-p" id="scrollupto"><a href="#listen"><span class="glyphicon glyphicon-chevron-up"></span></a></p>
-                    <p class="description soundcloud-p">&nbsp;&nbsp;</p>
-                    <p class="description soundcloud-p" id="scrolldowntothankyou"><a href="#thank-you"><span class="glyphicon glyphicon-chevron-down"></span></a></p>
+                  <p class="description soundcloud-p" id="scrollupto"><a href="#index"><span class="glyphicon glyphicon-chevron-up"></span></a></p>
+                  <p class="description soundcloud-p" id="scrolldowntothankyou"><a href="#thank-you"><span class="glyphicon glyphicon-chevron-down"></span></a></p>
+
                 </center>
               </div>
             </div>
@@ -291,39 +240,6 @@
                       <section class="col-md-3"></section>
                       <section class="col-md-6">
                         <a href="mailto:booking@oddist.net"><h2>booking@oddist.net</h2></a>
-                        <!-- Original Contact Form -->
-                        <!-- <form name="contactform" method="post" action="_send_form_email.php">
-                              <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                                <input type="text" name="full_name" class="form-control" placeholder="Please enter your full name">
-                              </div> 
-                            
-                              </br>
-                                             
-                              <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-                                <input type="text" name="email" class="form-control" placeholder="you@yourdomain.com">
-                              </div>
-                            
-                              </br>
-                          
-                              <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-earphone"></span></span>
-                                <input type="text" name="telephone" class="form-control" placeholder="Please enter your phone number">
-                              </div>
-                            
-                              </br>
-                                              
-                              <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                                <textarea class="input-xxlarge col-md-6 form-control" name="comments" placeholder="Compose your message here."></textarea>
-                              </div>
-                            
-                              </br>
-
-                             <input type="submit" value="Send" id="submit-button" class="btn btn-md btn-success"><a href="_send_form_email.php"></a>
-                           </br>                
-                        </form> -->
                       </section>
 
                       <section class="col-md-3"></section>
@@ -359,7 +275,7 @@
 <!-- ############# BEGIN FOOTER ############# -->
           <footer>
             <div>
-              <p>&copy; 2015 <img class="img-footer" src="images/oddist-signature_white.png" alt="ODDIST signature" title="ODDIST signature" /></p>
+              <p>&copy; 20<?php echo date("y"); ?> <img class="img-logo" src="images/oddist-signature_white.png" alt="ODDIST signature" title="ODDIST signature" /></p>
             </div>
           </footer>
 <!-- ############# end FOOTER ############# -->
